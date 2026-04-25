a6fa21384d0473745b6029f9964d4685ebad9fe3
epicmohid <epicmohid@gmail.com>
2026-04-25 21:02:49 +0500

    delete gameobject.cpp

--- GameObject.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameObject.h"
-
-GameObject::GameObject(int startX, int startY) : x(startX), y(startY) {}
-
-GameObject::~GameObject() {}
-
-int GameObject::getX() const { return x; }
-int GameObject::getY() const { return y; }
